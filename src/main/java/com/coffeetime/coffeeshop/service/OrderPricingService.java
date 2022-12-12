1cef0eab3bf300dd9b696df65ab9994f55c0c359
Necmi KILIC <necmik@gmail.com>
2022-12-12 20:23:00 +0100

    refactor order amounts

--- src/main/java/com/coffeetime/coffeeshop/service/OrderPricingService.java
+++ src/main/java/com/coffeetime/coffeeshop/service/OrderPricingService.java
@@ -1,6 +1,8 @@
 package com.coffeetime.coffeeshop.service;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,40 +25,61 @@ public class OrderPricingService {
 	private static final BigDecimal minimumAmountForDiscount = BigDecimal.valueOf(12);
 	
 	public void setOrderAmounts(Order order) {
-		BigDecimal originalAmount = BigDecimal.ZERO; // Total amount of the order
-    	BigDecimal lowestLineAmount = BigDecimal.valueOf(1000000000); // Lowest amount of the order lines
-    	
-        for (OrderLine orderLine : order.getOrderLines()) {   
-        	// Calculate amount of the order line (coffee + toppings)
+		
+		Set<OrderLine> orderLines = order.getOrderLines();
+		
+		// Set amount of the order lines
+		orderLines.forEach(orderLine -> {   
         	BigDecimal orderLineAmount = orderLine.getToppings().stream()
                     .map(x -> x.getAmount())    
                     .reduce(orderLine.getCoffee().getAmount(), BigDecimal::add); 
             
         	orderLine.setTotalAmount(orderLineAmount);
-        	
-        	originalAmount = originalAmount.add(orderLineAmount);
-        	lowestLineAmount = lowestLineAmount.min(orderLineAmount);            
-        };
+        });
         
+        // Set total amount of the order
+        BigDecimal originalAmount = orderLines
+        		.stream()
+        		.map(x -> x.getTotalAmount())
+        		.reduce(BigDecimal.ZERO, BigDecimal::add);
         order.setOriginalAmount(originalAmount);
         
-        BigDecimal discountAmount = BigDecimal.ZERO;
+        BigDecimal discountedAmount = getDiscountedAmount(originalAmount, orderLines);
+        order.setDiscountedAmount(discountedAmount); 
         
-        // If the total amount is higher than minimumAmountForDiscount then discount amount is %25
+        if (originalAmount.compareTo(discountedAmount) != 0) {
+        	logger.info("Discount applied. New total amount is {}", order.getDiscountedAmount());
+        }        
+	}
+	
+	/**
+	 * 
+	 * @param originalAmount : Total amount of the order
+	 * @param orderLines : Order lines
+	 * @return 
+	 * 
+	 * The method returns discounted amount of the order if applied. Otherwise returns the original amount.
+	 * Discount amount is decided by total amount of the order and cheapest order line (coffee).
+	 * 1) If the total amount is higher than minimumAmountForDiscount then %25 discount is applied.
+	 * 2) If there are 3 or more coffees in order, then the discount amount will be the cheapest one.
+	 * 3) If both conditions are eligible, then the lower discount is applied.
+	 */
+    private BigDecimal getDiscountedAmount(BigDecimal originalAmount, Set<OrderLine> orderLines) {
+    	BigDecimal discountAmount = BigDecimal.ZERO;
+        
         if (originalAmount.compareTo(minimumAmountForDiscount) > 0) {
         	discountAmount = originalAmount.multiply(BigDecimal.valueOf(discountPercentage));
         }
         
-        // If there are 3 or more coffees in order, then the cheapest one is free
-        if (order.getOrderLines().size() >= 3) {
-        	discountAmount = discountAmount.min(lowestLineAmount);
+        OrderLine cheapestOrderLine = orderLines
+        		.stream()
+        		.min(Comparator.comparing(OrderLine::getTotalAmount))
+        		.get();
+        
+        if (orderLines.size() >= 3) {
+        	discountAmount = discountAmount.min(cheapestOrderLine.getTotalAmount());
         }
         
-        // Select the minimum discount to apply to the order
-        order.setDiscountedAmount(originalAmount.subtract(discountAmount)); 
-        
-        if (originalAmount.compareTo(discountAmount) != 0) {
-        	logger.info("Discount applied. New price is {}", order.getDiscountedAmount());
-        }
-	}
+        return originalAmount.subtract(discountAmount); 
+    }
 }
